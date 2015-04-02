d0fdf892d4160121c5acf180bc529802be2cf1d0
Ethan Nadler <Nadler_ethan@bah.com>
2015-04-02 10:11:54 -0400

    initial commit

---
+++ MSBandHealth/Models/Pulse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MSBandHealth.Models
+{
+    public class Pulse
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Time { get; set; }
+        public int BPM { get; set; }
+    }
+}
